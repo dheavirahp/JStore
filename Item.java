fb6535f49f8babbf939b818dcf06dc56c07ee224
Dheavira Putri <47661005+dheavirahp@users.noreply.github.com>
2019-02-21 17:09:14 +0700

    tes

---
+++ Item.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Item{
+	private int id;
+	private String name;
+	private int stock;
+	private int price;
+	private String category;
+
+	public Item(){
+	}
+
+	public int getId (){
+		return id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public int getStock(){
+		return stock;
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+	public String getCategory (){
+		return category;
+	}
+
+	public setId (int id){
+		return 0;
+	}
+
+	public setName(String name){
+		
+	}
+
+	public setStock (int stock){
+		return 0;
+	}
+
+	public setPrice (int price){
+		return 0;
+	}
+
+	public setCategory (String category){
+
+	}
+}
---
+++ JStore.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class JStore {
+  public static void main(String[] args){
+  	public JStore(){
+  		
+  	}
+  }
